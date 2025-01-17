9ba6b121dd5b978caa9521ce87b57c67236e6eba
Esteban Trujillo <144358984+EstebanAdso@users.noreply.github.com>
2025-01-17 11:13:27 -0500

    agregando perfil de dev y prod

--- src/main/java/com/example/compuservicessoft/CompuServicesSoftApplication.java
+++ src/main/java/com/example/compuservicessoft/CompuServicesSoftApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,22 +15,43 @@ public class CompuServicesSoftApplication {
         SpringApplication.run(CompuServicesSoftApplication.class, args);
     }
 
-
+    // Configuración para el entorno de desarrollo
     @Configuration
-    public static class Myconfiguration {
+    @Profile("dev")
+    public static class DevCorsConfiguration {
         @Bean
         public WebMvcConfigurer corsConfigurer() {
             return new WebMvcConfigurer() {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOrigins("https://compuservicessoft.com")
+                            .allowedOrigins("http://127.0.0.1:5501", "http://localhost:5501")
                             .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
                             .allowedHeaders("*")
-                            .allowCredentials(true); // Habilita credenciales (si usas sesiones o cookies)
+                            .allowCredentials(true);
                 }
             };
         }
     }
 
+
+    // Configuración para el entorno de producción
+    @Configuration
+    @Profile("prod") // Solo se activa en el perfil "prod"
+    public static class ProdCorsConfiguration {
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("https://compuservicessoft.com") // Permitir solo tu dominio en producción
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                            .allowedHeaders("*")
+                            .allowCredentials(true);
+                }
+            };
+        }
+    }
 }
+
