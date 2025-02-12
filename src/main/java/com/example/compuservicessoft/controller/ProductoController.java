19a48223d6d220adf7412ef632120962451960c3
Esteban Trujillo <144358984+EstebanAdso@users.noreply.github.com>
2025-02-12 15:58:33 -0500

    mejorando los logs

--- src/main/java/com/example/compuservicessoft/controller/ProductoController.java
+++ src/main/java/com/example/compuservicessoft/controller/ProductoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("producto")
@@ -16,16 +17,20 @@ public class ProductoController {
     @Autowired
     private ProductoServices productoServices;
 
+    private static final Logger logger = Logger.getLogger(ProductoController.class.getName());
+
     @GetMapping("/categoria/{valor}")
     public ResponseEntity<List<Producto>> obtenerProductosPorCategoria(@PathVariable String valor) {
         try {
             Long categoriaId = Long.parseLong(valor);
+            logger.info("Categoría seleccionada por ID: " + categoriaId);
             List<Producto> productos = productoServices.findByCategoriaId(categoriaId);
             if (productos.isEmpty()) {
                 return ResponseEntity.notFound().build();
             }
             return ResponseEntity.ok(productos);
         } catch (NumberFormatException e) {
+            logger.info("Categoría seleccionada por Nombre: " + valor);
             List<Producto> productos = productoServices.findByCategoriaNombre(valor);
             if (productos.isEmpty()) {
                 return ResponseEntity.notFound().build();
@@ -34,15 +39,15 @@ public class ProductoController {
         }
     }
 
-
-
     @GetMapping
     public List<Producto> listar() {
+        logger.info("Listando todos los productos");
         return productoServices.findAll();
     }
 
     @GetMapping("{id}")
     public Optional<Producto> buscar(@PathVariable Long id) {
+        logger.info("Buscando producto con ID: " + id);
         return productoServices.findById(id);
     }
 }
